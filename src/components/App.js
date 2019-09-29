61a3f8cca9a508edbbf5ce516989e55413ef0415
Tri Hariyanto <macbookair@TriHariyanto.local>
2019-09-29 12:38:39 +0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Screen from '../containers/Screen';
+import ButtonList from '../containers/ButtonList';
+import History from '../containers/History';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <h3 className="mt-4"><i className="fa fa-calculator" />Calculator using React Redux</h3>
+          </div>
+          <div className="col-sm-8">
+            <Screen />
+          </div>
+          <div className="col-sm-8">
+            <h5>Buttons</h5>
+            <ButtonList />
+          </div>
+          <div className="col-sm-4">
+            <History />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/reducers/Index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import calculator from './Calculator';
+
+export default combineReducers({
+  calculator
+})
